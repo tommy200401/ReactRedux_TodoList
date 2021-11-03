592fcf4f1eb94b753668d5220ec8c6b1b988178c
Tommy Cheung <tommy200401@gmail.com>
2021-11-03 21:59:00 +0800

    [TodoList][Tommy] feat: updated delete item with API

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
 import '../style/TodoItem.css'
 import { useDispatch } from "react-redux";
 import classNames from 'classnames';
-import { updateTodo } from '../apis/todos';
+import { deleteTodo, updateTodo } from '../apis/todos';
 import { Button } from "antd";
 import {CloseOutlined, EditOutlined} from '@ant-design/icons'
 import EditTodoBox from './EditTodoBox';
@@ -11,20 +11,20 @@ const TodoItem = ({todo}) => {
     
     const dispatch = useDispatch()
 
-    const updateStatus = () => {
+    const updateStatus = () => {                          // anonymous
         updateTodo({ id: todo.id, done: !todo.done })     // unchange original text, pass id and different status
         .then(response=>(dispatch({type: 'todo/updateStatus', payload:response.data})))
     }
 
-    const deleteTodo = (id) => {
-        // api.delete(`/todos/${id}`)
-        dispatch({type: 'todo/delete', payload:todo.id})
+    const deleteTodoItem = () => {
+        deleteTodo(todo.id)
+        .then(response=>(dispatch({type:'todo/delete', payload:todo.id})));
     }
 
     return (
         <div className='box'>
             <span className={textClass} onClick={updateStatus}>{todo.text}</span>
-            <Button type="text" icon={<CloseOutlined style={{color: 'grey'}}/>} className="delete" onClick={deleteTodo}></Button>
+            <Button type="text" icon={<CloseOutlined style={{color: 'grey'}}/>} className="delete" onClick={deleteTodoItem}></Button>
             <Button type="text" icon={<EditOutlined/>} className="edit" onClick={EditTodoBox}></Button>
         </div>
     )
